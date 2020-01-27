39eb98a4708dea9ee965a6935dd6ed0aa72735a2
yu7a21 <hokusoukeisei@gmail.com>
2020-01-27 14:51:32 +0900

    add page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/' => "home#top"
+  get '/send' => "home#send"
+  get '/login' => "home#login"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
